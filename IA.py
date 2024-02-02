980196dbd46f991becdadf9e48eb230eb19495a2
greg-ogs <alexorogom@gmail.com>
2024-02-02 00:50:25 -0600

    Dataset done

--- IA.py
+++ IA.py
@@ -9,92 +9,122 @@ from tensorflow.keras.models import Sequential
 
 import pathlib
 
-data_dir = "E:\spneurodiffuse\dataset"
 
-# image_count = len(list(data_dir.glob('*/*.jpg')))
-# print(image_count)
+class BackPropagation():
+    def __init__(self):
+        self.class_names = None
+        self.batch_size = 32
+        self.img_height = 180
+        self.img_width = 180
+        data_dir = "E:\spneurodiffuse\dataset"
 
-batch_size = 32
-img_height = 180
-img_width = 180
+        # image_count = len(list(data_dir.glob('*/*.jpg')))
+        # print(image_count)
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+        self.train_ds = tf.keras.utils.image_dataset_from_directory(
+            data_dir,
+            validation_split=0.2,
+            subset="training",
+            seed=123,
+            image_size=(self.img_height, self.img_width),
+            batch_size=self.batch_size)
 
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+        self.val_ds = tf.keras.utils.image_dataset_from_directory(
+            data_dir,
+            validation_split=0.2,
+            subset="validation",
+            seed=123,
+            image_size=(self.img_height, self.img_width),
+            batch_size=self.batch_size)
 
-class_names = train_ds.class_names
+        self.class_names = self.train_ds.class_names
 
-AUTOTUNE = tf.data.AUTOTUNE
+    def train_model(self):
+        AUTOTUNE = tf.data.AUTOTUNE
 
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+        train_ds = self.train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+        val_ds = self.val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-normalization_layer = layers.Rescaling(1./255)
+        normalization_layer = layers.Rescaling(1. / 255)
 
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-# Notice the pixel values are now in `[0,1]`.
-print(np.min(first_image), np.max(first_image))
+        normalized_ds = self.train_ds.map(lambda x, y: (normalization_layer(x), y))
+        image_batch, labels_batch = next(iter(normalized_ds))
+        first_image = image_batch[0]
+        # Notice the pixel values are now in `[0,1]`.
+        print(np.min(first_image), np.max(first_image))
 
-num_classes = len(class_names)
+        num_classes = len(self.class_names)
 
-model = Sequential([
-    layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-    layers.Conv2D(16, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(32, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(num_classes)
-])
+        model = Sequential([
+            layers.Rescaling(1. / 255, input_shape=(self.img_height, self.img_width, 3)),
+            layers.Conv2D(16, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Flatten(),
+            layers.Dense(128, activation='relu'),
+            layers.Dense(num_classes)
+        ])
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+        model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
 
-model.summary()
+        model.summary()
 
-epochs=7
-history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
-)
+        epochs = 5
+        history = model.fit(
+            self.train_ds,
+            validation_data=self.val_ds,
+            epochs=epochs
+        )
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+        acc = history.history['accuracy']
+        val_acc = history.history['val_accuracy']
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
 
-epochs_range = range(epochs)
+        epochs_range = range(epochs)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+        plt.figure(figsize=(8, 8))
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs_range, acc, label='Training Accuracy')
+        plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+        plt.legend(loc='lower right')
+        plt.title('Training and Validation Accuracy')
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, loss, label='Training Loss')
+        plt.plot(epochs_range, val_loss, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+        plt.show()
+
+        model.save('my_model.keras')
+
+        input("Enter to continue")
+
+        self.predict()
+
+    def predict(self):
+        img = tf.keras.utils.load_img(
+            "E:\spneurodiffuse\dataset\downleft\IMG_0.jpg", target_size=(self.img_height, self.img_width)
+        )
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)
+        model = tf.keras.models.load_model('my_model.keras')
+        predictions = model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
+
+        print(
+            "This image most likely belongs to {} with a {:.2f} percent confidence."
+            .format(self.class_names[np.argmax(score)], 100 * np.max(score))
+        )
+
+
+if __name__ == "__main__":
+    mod = BackPropagation()
+    mod.train_model()
