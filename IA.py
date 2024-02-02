9172a81a0443af8eb5824d343e2fb660a26baa4e
greg-ogs <alexorogom@gmail.com>
2024-02-01 22:35:51 -0600

    Dataset done

--- IA.py
+++ IA.py
@@ -9,12 +9,10 @@ from tensorflow.keras.models import Sequential
 
 import pathlib
 
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file('flower_photos.tar', origin=dataset_url, extract=True)
-data_dir = pathlib.Path(data_dir).with_suffix('')
+data_dir = "E:\spneurodiffuse\dataset"
 
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
+# image_count = len(list(data_dir.glob('*/*.jpg')))
+# print(image_count)
 
 batch_size = 32
 img_height = 180
@@ -72,7 +70,7 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-epochs=10
+epochs=7
 history = model.fit(
     train_ds,
     validation_data=val_ds,
