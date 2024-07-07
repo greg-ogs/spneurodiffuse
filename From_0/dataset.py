4b313c42951d8b3966deecd0bcbe091d98aff653
greg-ogs <alexorogom@gmail.com>
2024-07-07 15:21:56 -0600

    SQL connection

--- From_0/dataset.py
+++ From_0/dataset.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import os
+import mysql.connector as mysql
 
 
 # from tqdm.auto import tqdm
@@ -10,8 +11,8 @@ import os
 def create_base_dataset():
     # High RAM memory, low time
     print("Creating base dataset...")
-    x = np.arange(0, 25, 0.001)  # Creates an array from 0 to 25 for x
-    y = np.arange(0, 25, 0.001)  # Creates an array from 0 to 25 for y
+    x = np.arange(0, 25, 0.01)  # Creates an array from 0 to 25 for x
+    y = np.arange(0, 25, 0.01)  # Creates an array from 0 to 25 for y
     coords = np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
     coords = np.round(coords, 3)
     n_dataset = pd.DataFrame(coords)
@@ -29,35 +30,27 @@ def create_base_dataset():
     # print("Base dataset created.")
 
 
-class Dataset():
+class Dataset:
     def __init__(self):
-        pass
+        self.dataset = None
+        self.file_path = None
+
+        # MySQL
+        self.mydb = mysql.connect(
+            host="172.17.0.2",
+            user="user",
+            database="dataset",
+            password="userpass", port=3306
+        )
+
+        self.mycursor = self.mydb.cursor()
 
     # initial adjusment
     def load_dataset(self):
         try:
             self.file_path = 'base_dataset.csv'
-            # chunksize = 100000
-            # it = 0
-            # start_time = time.time()
-            # for chunk in pd.read_csv(self.file_path, chunksize=chunksize):
-            #     it = it + 1
-            #     current_time = time.time()  # Time after each chunk
-            #     elapsed_time = current_time - start_time
-            #     print(f"Elapsed Time: {elapsed_time:.2f} seconds | Rows Processed: {chunksize*it} | Iterations: {it}")
-            # total_rows = len(pd.concat(pd.read_csv(self.file_path, chunksize=chunksize)))
-            # print(f"\nTotal Rows: {total_rows}")
-            # end_time = time.time()
-            # total_read_time = end_time - start_time
-            # print(f"Total Read Time: {total_read_time:.2f} seconds")
-
-            # Base read
-            # start_time = time.time()
             self.dataset = pd.read_csv(self.file_path)
             print(self.dataset.shape)
-            # end_time = time.time()
-            # read_time = end_time - start_time
-            # print(read_time)
 
         except FileNotFoundError:
 
@@ -76,14 +69,20 @@ class Dataset():
 
             print(f"Unexpected error while reading '{self.file_path}': {e}")
 
-    def insert_bool(self):
-        # for example the firs iteration the win coordinate is 10.246, 15.270
-        win_coords = [0.0, 0.0]
-        mask1 = self.dataset.iloc[:, 0] == win_coords[0]  # Assuming column names or positions
-        mask2 = self.dataset.iloc[:, 1] == win_coords[1]
-        self.dataset['matched'] = mask1 & mask2
-        self.dataset.head()
+    def insert_bool(self, x_d, y_d, x_u, y_u):
+        query = "UPDATE base_dataset SET RESULT = %s WHERE C1 > %s AND C2 > %s AND C1 < %s AND C2 < %s"
+        val = (1, x_d, y_d, x_u, y_u)
+        self.mycursor.execute(query, val)
+        self.mydb.commit()
 
 
 if __name__ == '__main__':
     dataset = Dataset()
+    # dataset.load_dataset()
+    x = 15.05
+    y = 10.05
+    xd = round(x - 0.1, 2)
+    yd = round(y - 0.1, 2)
+    xu = round(x + 0.1, 2)
+    yu = round(y + 0.1, 2)
+    dataset.insert_bool(xd, yd, xu, yu)
