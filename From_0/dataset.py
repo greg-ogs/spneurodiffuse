8debf3137a18589d0a6db0b4f2e50151c31944cf
greg-ogs <alexorogom@gmail.com>
2024-07-19 04:21:39 -0700

    Starting the README and dataset alternative option after last results using 6.7 millions of unique data. The new dataset will be build with a range of coordinates based in a true coordinate.

--- From_0/dataset.py
+++ From_0/dataset.py
@@ -7,30 +7,65 @@ import mysql.connector as mysql
 
 # from tqdm.auto import tqdm
 
+class Datasets:
+    def __init__(self):
+        self.yv = None
+        self.xv = None
+    @staticmethod
+    def create_base_dataset():  # Create a dataset using all possible combinations
+        # High RAM memory, low time
+        print("Creating base dataset...")
+        x = np.arange(0, 25, 0.1)  # Creates an array from 0 to 25 for x
+        y = np.arange(0, 25, 0.1)  # Creates an array from 0 to 25 for y
+        coords = np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
+        coords = np.round(coords, 3)
+        n_dataset = pd.DataFrame(coords)
+        n_dataset.head(100)
+        n_dataset.to_csv("base_dataset.csv", index=False)
+        print("Base dataset created. Please run again")
 
-def create_base_dataset():
-    # High RAM memory, low time
-    print("Creating base dataset...")
-    x = np.arange(0, 25, 0.1)  # Creates an array from 0 to 25 for x
-    y = np.arange(0, 25, 0.1)  # Creates an array from 0 to 25 for y
-    coords = np.array(np.meshgrid(x, y)).T.reshape(-1, 2)
-    coords = np.round(coords, 3)
-    n_dataset = pd.DataFrame(coords)
-    n_dataset.head(100)
-    n_dataset.to_csv("base_dataset.csv", index=False)
-    print("Base dataset created. Please run again")
+        # Low memory but long time
+        # n_dataset = [[10, 10]]
+        # x = np.arange(0, 25, 0.001)
+        # y = np.arange(0, 25, 0.001)
+        # coords_generator = ((xi, yi) for xi in x for yi in y)  # Generator object is returned
+        # for coordinate in tqdm(coords_generator, desc="Creating base dataset", ascii=True, total=625000000):
+        #     n_dataset = np.vstack((n_dataset, np.asarray(coordinate)))
+        # print("Base dataset created.")
 
-    # Low memory but long time
-    # n_dataset = [[10, 10]]
-    # x = np.arange(0, 25, 0.001)
-    # y = np.arange(0, 25, 0.001)
-    # coords_generator = ((xi, yi) for xi in x for yi in y)  # Generator object is returned
-    # for coordinate in tqdm(coords_generator, desc="Creating base dataset", ascii=True, total=625000000):
-    #     n_dataset = np.vstack((n_dataset, np.asarray(coordinate)))
-    # print("Base dataset created.")
+    @staticmethod
+    def create_dynamic_range(coord):
 
+        if coord >= 1: lxd = coord - 1; dr = 0
+        else: lxd = 0; dr = 1-coord;
+        if coord <= 24: lxu = coord + 1; ur = 0
+        else: lxu = 25; ur = (coord + 1) - 25;
+        lxd = lxd - ur
+        lxu = lxu + dr
 
-class Dataset:
+        xv = np.arange(lxd, lxu, 0.01)
+        return xv
+
+    def concatenate_coordinates(self, x, y):
+        self.xv = self.create_dynamic_range(x)
+        self.yv = self.create_dynamic_range(y)
+
+        print('X vector: {}'.format(self.xv))
+        print('Y vector: {}'.format(self.yv))
+
+        aux = 0
+        for coordinates in zip(self.xv, self.yv):
+            print("Pair of coordinates: {}, {}".format(round(coordinates[0], 3), round(coordinates[1], 3)))
+            aux = aux + 1
+            # add mysql query to insert the columns
+
+        print(aux)
+        if x <= 1 or y <= 1 or x >= 24 or y >= 24:
+            print("Pair of real coordinates: {}, {}".format(round(x, 3), round(y, 3)))
+            # add mysql query to insert the original pair of coordinates several times
+            pass
+
+class ReadDataset:
     def __init__(self):
         self.dataset = None
         self.file_path = None
@@ -46,7 +81,7 @@ class Dataset:
         self.mycursor = self.mydb.cursor()
 
     # initial adjusment
-    def load_dataset(self):
+    def load_dataset_from_csv(self):
         try:
             self.file_path = 'base_dataset.csv'
             self.dataset = pd.read_csv(self.file_path)
@@ -55,7 +90,6 @@ class Dataset:
         except FileNotFoundError:
 
             print(f"Error: File '{self.file_path}' not found.")
-            create_base_dataset()
 
         except pd.errors.EmptyDataError:
 
@@ -77,13 +111,15 @@ class Dataset:
 
 
 if __name__ == '__main__':
-    dataset = Dataset()
-    # dataset.load_dataset()
-    x = 17.9
-    y = 12.8
-    # elt = int(np.round((np.random.rand() * (864000 - 86400 + 1) + 86400), 0))
-    xd = round(x - 0.3, 1)
-    yd = round(y - 0.3, 1)
-    xu = round(x + 0.3, 1)
-    yu = round(y + 0.3, 1)
-    dataset.insert_bool(xd, yd, xu, yu)
+    newDataset = Datasets()
+    newDataset.concatenate_coordinates(12.5, 24.6)
+
+    # dataset = ReadDataset()
+    # To update data into mysql server table
+    # x = 17.9
+    # y = 12.8
+    # xd = round(x - 0.3, 1)
+    # yd = round(y - 0.3, 1)
+    # xu = round(x + 0.3, 1)
+    # yu = round(y + 0.3, 1)
+    # dataset.insert_bool(xd, yd, xu, yu)
